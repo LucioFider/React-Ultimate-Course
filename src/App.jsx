50fa0b9ab9b77f4d151fe765312d3cbe626a053d
LucioFider <fidelislucio404@gmail.com>
2023-03-16 10:43:11 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
